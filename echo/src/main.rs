3f8397226be82ea18e57f75bd0b64d97667bb59f
MightiD <128378210+MightiD@users.noreply.github.com>
2025-08-12 13:12:42 +0100

    made code better

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -19,26 +19,20 @@ struct Cli {
     input: Vec<String>,
 }
 
-fn create_string_from_args(args: &Vec<String>) -> String {
-
-    //must be from first part of input
-    //as it would add nothing to args[1]
-    //resulting in a leading space
-    let mut input = String::from(&args[1]);
-
-    for (i, arg) in args[2..].iter().enumerate() {
-        //probably not the most efficient way to add 2 strings
-        //but it works
-        input = format!("{} {}", input, arg);
-    }
-
-    input
-
-}
-
 fn main() {
     let args = Cli::parse();
-    let input_string = create_string_from_args(&args.input);
-    dbg!(args);
-    println!("{input_string}");
+
+    for word in args.input.iter() {
+        // need trailing white space so doesnt put words together
+        print!("{} ", word)
+    }
+
+    // TODO: make not have trailing whitespace on last word
+
+    if !args.n {
+        println!("")
+    }
+
+    // dbg!(args);
+    // println!("{input_string}");
 }
